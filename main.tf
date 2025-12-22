8846d721e6c66cd580e91997caf0fbfd8f268bd7
xnoto <steven@makeitwork.cloud>
2025-12-21 21:04:14 -0700

    remove: torwww VM, migrating to k8s tor-proxy

--- main.tf
+++ main.tf
@@ -31,20 +31,4 @@ module "runner" {
   aap_inventory_name                = "libvirt"
 }
 
-module "torwww" {
-  source                            = "git::https://github.com/makeitworkcloud/terraform-libvirt-domain.git"
-  name                              = "torwww"
-  description                       = "Tor hidden service web mirror"
-  memory                            = 4096
-  boot_image_url                    = local.boot_image_url
-  cloudinit_meta_data_template      = "${path.module}/cloud-init/meta_data.cfg"
-  cloudinit_meta_data_vars          = { hostname = "torwww" }
-  cloudinit_user_data_template      = "${path.module}/cloud-init/torwww/cloud_init.cfg"
-  cloudinit_user_data_vars          = { ssh_authorized_key = data.sops_file.secret_vars.data["ssh_admin_pubkey"] }
-  cloudinit_network_config_template = "${path.module}/cloud-init/network_config.cfg"
-  cloudinit_network_config_vars     = { private_ip_addr = data.sops_file.secret_vars.data["torwww_ip_addr"] }
-  private_ip_addr                   = data.sops_file.secret_vars.data["torwww_ip_addr"]
-  proxyhost                         = data.sops_file.secret_vars.data["proxyhost"]
-  enable_aap                        = true
-  aap_inventory_name                = "libvirt"
-}
+
